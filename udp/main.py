230c9e34a659469caa0307b7fba6809f82c2e353
Lars Ivar Hatledal <laht@ntnu.no>
2022-12-14 13:41:57 +0100

    update udp

--- udp/main.py
+++ udp/main.py
@@ -1,24 +1,33 @@
-from pymodbus.client.sync import ModbusTcpClient
+import socket
+import pynmea2
 
+UDP_IP = "0.0.0.0"  # Listen on all available network interfaces
+UDP_PORT = 15101 
 
 def main()
     
+    # Create a UDP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    # Create a Modbus client object
-    client = ModbusTcpClient('localhost', port=502)
+    # Bind the socket to the IP and port
+    sock.bind((UDP_IP, UDP_PORT))
 
-    # Connect to the Modbus server
-    client.connect()
-    
-    # read registers 1-4
-    result = client.read_holding_registers(1, 4, unit=0x01)
-    print(result.registers)
+    while True:
+        # Receive data from the socket
+        data, addr = sock.recvfrom(1024)  # 1024 is the maximum size of a UDP packet
 
-    # write registers 101-106
-    #client.write_registers(101, [0, 0, 0, 0, 0, 0], unit=0x01)
+        # Parse the NMEA message using pynmea2
+        try:
+            msg = pynmea2.parse(data.decode("utf-8"))
+        except pynmea2.ParseError:
+            # The data is not a valid NMEA message, skip it
+            continue
 
-    # Close the connection
-    client.close()
+        # At this point, you can access the individual fields of the NMEA message
+        # using the properties of the `msg` object. For example:
+        print(f"Timestamp: {msg.timestamp}")
+        print(f"Latitude: {msg.lat}")
+        print(f"Longitude: {msg.lon}")
 
 if __name__=="__main__":
     main()
