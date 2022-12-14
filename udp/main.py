e20ce3518b64ccfbef29ec958619a9df07cb287f
Lars Ivar Hatledal <laht@ntnu.no>
2022-12-14 13:48:20 +0100

    update

--- udp/main.py
+++ udp/main.py
@@ -4,30 +4,34 @@ import pynmea2
 UDP_IP = "0.0.0.0"  # Listen on all available network interfaces
 UDP_PORT = 15101 
 
+
 def main():
     
-    # Create a UDP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # Create a UDP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    # Bind the socket to the IP and port
-    sock.bind((UDP_IP, UDP_PORT))
+        # Bind the socket to the IP and port
+        sock.bind((UDP_IP, UDP_PORT))
 
-    while True:
-        # Receive data from the socket
-        data, addr = sock.recvfrom(1024)  # 1024 is the maximum size of a UDP packet
+        while True:
+            # Receive data from the socket
+            data, addr = sock.recvfrom(1024)  # 1024 is the maximum size of a UDP packet
 
-        # Parse the NMEA message using pynmea2
-        try:
-            msg = pynmea2.parse(data.decode("utf-8"))
-        except pynmea2.ParseError:
-            # The data is not a valid NMEA message, skip it
-            continue
+            # Parse the NMEA message using pynmea2
+            try:
+                msg = pynmea2.parse(data.decode("utf-8"))
+            except pynmea2.ParseError:
+                # The data is not a valid NMEA message, skip it
+                continue
 
-        # At this point, you can access the individual fields of the NMEA message
-        # using the properties of the `msg` object. For example:
-        print(f"Timestamp: {msg.timestamp}")
-        print(f"Latitude: {msg.lat}")
-        print(f"Longitude: {msg.lon}")
+            # At this point, you can access the individual fields of the NMEA message
+            # using the properties of the `msg` object. For example:
+            print(f"Timestamp: {msg.timestamp}")
+            print(f"Latitude: {msg.lat}")
+            print(f"Longitude: {msg.lon}")
+    except Exception as e:
+        print(e)
 
 if __name__=="__main__":
     main
