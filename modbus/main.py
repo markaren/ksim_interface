813d5cd4c49e6926fbe165315a982e9f7645c332
Lars Ivar Hatledal <laht@ntnu.no>
2022-12-14 13:10:07 +0100

    fix error and catch exception

--- modbus/main.py
+++ modbus/main.py
@@ -1,24 +1,27 @@
 from pymodbus.client.sync import ModbusTcpClient
 
 
-def main()
+def main():
     
+    try:
+        # Create a Modbus client object
+        client = ModbusTcpClient('localhost', port=502)
 
-    # Create a Modbus client object
-    client = ModbusTcpClient('localhost', port=502)
+        # Connect to the Modbus server
+        client.connect()
+        
+        # read registers 1-4
+        result = client.read_holding_registers(1, 4, unit=0x01)
+        print(result.registers)
 
-    # Connect to the Modbus server
-    client.connect()
+        # write registers 101-106
+        #client.write_registers(101, [0, 0, 0, 0, 0, 0], unit=0x01)
+
+        # Close the connection
+        client.close()
+    except Exception as e:
+        print(e)
     
-    # read registers 1-4
-    result = client.read_holding_registers(1, 4, unit=0x01)
-    print(result.registers)
-
-    # write registers 101-106
-    #client.write_registers(101, [0, 0, 0, 0, 0, 0], unit=0x01)
-
-    # Close the connection
-    client.close()
 
 if __name__=="__main__":
     main()
