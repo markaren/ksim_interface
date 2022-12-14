78c568344daea80780cb743514316ca88c285e3c
Lars Ivar Hatledal <laht@ntnu.no>
2022-12-14 12:33:12 +0100

    update

---
+++ udp/README.md
@@ -0,0 +1,5 @@
+# udp_sim_interface
+
+Simulator transmits NMEA messages over UDP
+
+`pip install pynmea2`
---
+++ udp/main.py
@@ -0,0 +1,24 @@
+from pymodbus.client.sync import ModbusTcpClient
+
+
+def main()
+    
+
+    # Create a Modbus client object
+    client = ModbusTcpClient('localhost', port=502)
+
+    # Connect to the Modbus server
+    client.connect()
+    
+    # read registers 1-4
+    result = client.read_holding_registers(1, 4, unit=0x01)
+    print(result.registers)
+
+    # write registers 101-106
+    #client.write_registers(101, [0, 0, 0, 0, 0, 0], unit=0x01)
+
+    # Close the connection
+    client.close()
+
+if __name__=="__main__":
+    main()
