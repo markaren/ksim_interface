21ff5ccf6ede5662e70829ac0a23d24ac1e961aa
Lars Ivar Hatledal <laht@ntnu.no>
2023-02-09 11:26:09 +0100

    wip

--- ws/WebsocketServer.py
+++ ws/WebsocketServer.py
@@ -1,8 +1,10 @@
 
 import json
+import socket
 import asyncio
 import websockets
 
+from threading import Thread
 from modbus.Simulator import Simulator, DummySimulator
 from modbus.json_types import ReadRequest, WriteRequest
 
@@ -10,9 +12,39 @@ from modbus.json_types import ReadRequest, WriteRequest
 class Handler:
 
     def __init__(self):
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sim = DummySimulator(("", 0))
         # self.sim = Simulator(("0.0.0.0", 502))
         self.subs = []
+
+        self.loop = asyncio.get_event_loop()
+
+        def udp():
+
+            async def publish(websocket, data):
+                await websocket.send(data)
+
+            try:
+                self.sock.bind(("0.0.0.0", 15101))
+
+                while True:
+                    data, addr = self.sock.recvfrom(256)
+                    dataStr = data.decode("utf-8")
+                    tasks = []
+                    for sub in self.subs:
+                        tasks.append(asyncio.create_task(publish(sub, dataStr)))
+                    self.loop.run_until_complete(asyncio.gather(*tasks))
+            except:
+                self.sock = None
+
+        udpThread = Thread(target=udp)
+        udpThread.start()
+
+    def stop(self):
+        if self.sock is not None:
+            self.sock.close()
+
 
     async def handleRequest(self, websocket):
         async for message in websocket:
@@ -22,7 +54,7 @@ class Handler:
                 values = self.sim.makeRequest(ReadRequest.fromJSON(msg))
                 try:
                     await websocket.send(json.dumps(values))
-                except Exception:
+                except:
                     if websocket in self.subs:
                         self.subs.remove(websocket)
 
@@ -38,6 +70,7 @@ async def main():
     def wait():
         print("Press any key to exit..")
         input()
+        handler.stop()
 
     async with websockets.serve(handler.handleRequest, "0.0.0.0", 8765):
         loop = asyncio.get_event_loop()
