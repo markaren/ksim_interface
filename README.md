9b56352679be7a521aa3304d31f0a19b79a2c044
Lars Ivar Hatledal <laht@ntnu.no>
2023-02-09 13:08:40 +0100

    update

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 Test setup for interfacing with the Kongberg research simulator at NTNU
 
-## modbus interface
+### modbus interface
 
 See modbus folder
 
-## UDP interface (NMEA)
+### UDP interface (NMEA)
 
 See udp folder
+
+### Unified Websocket interface
+
+See ws folder
--- udp/main.py
+++ udp/main.py
@@ -1,42 +1,24 @@
 import time
 import socket
-import pynmea2
-
-UDP_IP = "0.0.0.0"  # Listen on all available network interfaces
-UDP_PORT = 15101 
 
 
 def main():
     
     try:
-        # Create a UDP socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-        # Bind the socket to the IP and port
-        sock.bind((UDP_IP, UDP_PORT))
+        sock.bind(("0.0.0.0", 15101))
 
         while True:
             # Receive data from the socket
-            data, addr = sock.recvfrom(256)  # 1024 is the maximum size of a UDP packet
-
+            data, addr = sock.recvfrom(256)
             dataStr = data.decode("utf-8")
-            
-            if dataStr.startswith("$"):
-                try:
-                    # Parse the NMEA message using pynmea2
-                    msg = pynmea2.parse(dataStr)
-                    print(msg)
-                except pynmea2.ParseError as e:
-                    # The data is not a valid NMEA message, skip it
-                    print(e)
-                    break
-            else:
-                print(dataStr)
+            print(dataStr)
             
             time.sleep(0.1)
 
     except Exception as e:
         print(e)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
